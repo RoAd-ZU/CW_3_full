a65715f18b2d30f557edf5d0d81e9f6185d58c37
RoAd-ZU <ZUZERN.AD@gmail.com>
2023-08-15 02:48:07 +0300

    CW3 full

---
+++ utils.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from load_json import sorting
+
+
+def get_five_operations(sort):
+    """Возвращает пять последних операций в порядке от последней, по дате"""
+    sort = sort[-5:]
+    sort.reverse()
+    return sort
+
+
+#
+def hide_symbols(operations):
+    """Маскирует символы в операциях пользователя, проверяя первый символ в значении словаря"""
+
+    for element in operations:
+        if element['to'][0] == 'С':
+            element['to'] = 'Счет ' + '**' + element['to'][-4:]
+        else:
+            element['to'] = element['to'][:-12] + ' ' + element['to'][-12:-10] + '** ' + '**** ' + element['to'][-4:]
+        if 'from' in element:
+            if element['from'][0] == 'С':
+                element['from'] = 'Счет ' + '**' + element['from'][-4:]
+            else:
+                element['from'] = element['from'][:-12] + ' ' + element['from'][-12:-10] + '** ' + '**** ' + element[
+                                                                                                                'from'][
+                                                                                                            -4:]
+        else:
+            continue
+
+    return operations
